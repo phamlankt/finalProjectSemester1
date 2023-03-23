cf9130ee71abe05062af867f4dba32533184be0f
Lan Pham <lan.pham@dfs.de>
2023-03-23 02:15:44 +0100

    updating CSS to display footer social icons to be in the same level with header logo and update JS to be able to close the menu when clicking in the body

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,35 @@ function openmenu(x) {
     console.log(display);
 
     if (display == "flex") {
-        // if (false) {
         document.getElementById("menu_bar_nav").style.display = "none";
-        document.getElementById("header1").style.setProperty("left", "0%");
+        document.getElementById("header").style.setProperty("left", "0%");
+        document.getElementById("footer").style.setProperty("left", "0%");
     } else {
         document.getElementById("menu_bar_nav").style.display = "flex";
-        document.getElementById("header1").style.setProperty("left", "254px");
+        document.getElementById("header").style.setProperty("left", "254px");
+        document.getElementById("footer").style.setProperty("left", "254px");
     }
 
 }
+
+function closemenu(x) {
+    var element = document.querySelector("#menu_bar_nav");
+    var display = getComputedStyle(element).display;
+    console.log(display);
+
+    if (display == "flex") {
+        document.getElementById("menu_bar_nav").style.display = "none";
+        document.getElementById("header").style.setProperty("left", "0%");
+        document.getElementById("footer").style.setProperty("left", "0%");
+    }
+}
+
+window.addEventListener('click', function (e) {
+    if (document.getElementById('menu_wrapper').contains(e.target)) {
+        openmenu();
+
+    } else {
+        closemenu();
+    }
+});
 
