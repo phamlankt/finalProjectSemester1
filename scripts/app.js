d9fd769b8c784af1d00499d2abb16143c076aa86
Lan Pham <lan.pham@dfs.de>
2023-04-01 04:54:59 +0200

    fixing download_app button onclick event, fixing li onclick event to open coresponding content/image

--- scripts/app.js
+++ scripts/app.js
@@ -30,68 +30,64 @@ function closemenu(x) {
         document.getElementById("main_content").style.setProperty("left", "23%");
     }
 }
+// listen to event to open and close menu
+window.addEventListener('click', function (e) {
+    if (document.getElementById('menu_wrapper').contains(e.target)) {
+        openmenu();
 
-// window.addEventListener('click', function (e) {
-//     if (document.getElementById('menu_wrapper').contains(e.target)) {
-//         openmenu();
-
-//     } else {
-//         closemenu();
-//     }
-//     // this.removeEventListener(this,'click');
-
-// });
-
-
-// click download app button to open download section --- NOT WORKING
-let download_app_button = document.getElementById('download_btn');
-download_app_button.onclick = open_download_section();
-
-function open_download_section() {
-    var home_section = document.getElementById('home_ctn');
-    var download_section = document.getElementById('download_ctn');
-    if (download_section.style.display == 'none') {
-        home_section.style.display = 'none';
-        download_section.style.display = 'block';
     } else {
-        download_section.style.display = 'none';
+        closemenu();
     }
-}
+
+});
+
+
+// click download app button to open download section
+let download_app_button = document.getElementById('download_btn');
+download_app_button.addEventListener('click', () => {
+    document.getElementById('home_ctn').classList.remove('active');
+    document.getElementById('download_ctn').classList.add('active');
+})
 
 
 let item_sections = document.getElementsByClassName("item");
 let active_items = document.getElementsByClassName('active');
 
 
-// Click li to display coresponding content---NOT WORKING
+// Click li to display coresponding content
 let list = document.querySelectorAll('#section_dot li');
 
 for (var i = 0; i < list.length; i++) {
-    console.log(list[i]);
     list[i].addEventListener('click', (e) => {
-        console.log(e.target.id);
+        active_items = document.getElementsByClassName('active');
+        let item_name = e.target.id.slice(e.target.id.indexOf('_') + 1);
+        let current_active_item = active_items[1].getAttribute('id').slice(active_items[1].getAttribute('id').indexOf('_') + 1);
+        // console.log("item_name: " + item_name);
+        // console.log("current_active_item: " + current_active_item);
+        if (item_name != current_active_item) {
+            removeCLassActive(current_active_item);
+            addCLassActive(item_name);
+        }
     })
 }
 
-
-for (let item of item_sections) {
-    let item_id = item.getAttribute('id');
-    let item_name = item_id.slice(item_id.indexOf('_') + 1);
-    // console.log(item_name);
-    item.addEventListener("click", displayItem(item_name));
+function addCLassActive(classname) {
+    let classItems = document.getElementsByClassName(classname);
+    for (let item of classItems) {
+        item.classList.add('active');
+    }
 }
 
-function displayItem(item_name) {
-    console.log(item_name);
-    // let items = document.getElementsByClassName(item_name);
-    // for (let item of items) {
-    //     item.setAttribute('class', 'active');
-    //     // console.log(item);
-    // }
-}
 
+function removeCLassActive(classname) {
+    let classItems = document.getElementsByClassName(classname);
+    for (let item of classItems) {
+        item.classList.remove('active');
+    }
+}
 
 function displayPreviousSibling() {
+    active_items = document.getElementsByClassName('active');
     for (let active_item of active_items) {
         if (active_item.previousElementSibling != null) {
             active_item.classList.remove('active');
@@ -101,6 +97,7 @@ function displayPreviousSibling() {
 }
 
 function displayNextSibling() {
+    active_items = document.getElementsByClassName('active');
     for (let active_item of active_items) {
         if (active_item.nextElementSibling != null) {
             active_item.classList.remove('active');
