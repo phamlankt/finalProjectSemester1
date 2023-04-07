ee027f8240de3bba85013ac36fe39481a3eef6ba
Lan Pham <lan.pham@dfs.de>
2023-04-07 16:04:00 +0200

    fixing the red line under each active tab

--- scripts/about.js
+++ scripts/about.js
@@ -1,9 +1,12 @@
 // Section about
 let li_tags = document.querySelectorAll('section.about ul li.about_li');
-for (let li_tag of li_tags) {
+console.log(li_tags);
+for (let i = 0; i < li_tags.length; i++) {
+    let li_tag = li_tags[i];
     li_tag.addEventListener('click', () => {
+        let shiftedOffset = i * 25;
+        document.getElementById("underscore_line").style.left = shiftedOffset + '%';
         let li_current_active = document.querySelectorAll('li.active_ctn');
-        console.log(li_current_active);
         for (let current_active of li_current_active) {
             let about_current_active_name = current_active.getAttribute('id').slice(current_active.getAttribute('id').indexOf('_') + 1);
             document.getElementById('about_' + about_current_active_name + '_ctn').classList.remove('active_ctn');
