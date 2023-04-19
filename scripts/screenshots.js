49e7346d96ee86a9ce8788417c4dedb4906d6dd2
Lan Pham <lan.pham@dfs.de>
2023-04-19 08:12:08 +0200

    cleaning up

--- scripts/screenshots.js
+++ scripts/screenshots.js
@@ -4,58 +4,7 @@ const elNavs = document.querySelectorAll('.screenshots ul.screenshots_controls l
 const slider = document.querySelector('div.wrapper_screenshot_imgs');
 screenshots[0].classList.add('scr_active');
 
-// console.log(document.querySelector('.scr_active'));
-// document.querySelector('.scr_active').classList.remove('.scr_active');
 elNavs[0].classList.add('scr_active');
-
-
-// const checkLastActive = () => {
-//     for (let i = 0; i < screenshots.length; i++) {
-//         if (screenshots[i].classList.contains('scr_active')) {
-//             screenshots[i].classList.remove('scr_active');
-//             elNavs[i].classList.remove('scr_active');
-//             if (i === screenshots.length - 1) {
-//                 screenshots[0].classList.add('scr_active');
-//                 elNavs[0].classList.add('scr_active');
-//                 return true;
-//             } else {
-//                 screenshots[i + 1].classList.add('scr_active');
-//                 elNavs[i + 1].classList.add('scr_active');
-//                 return false;
-//             }
-//         }
-//     }
-// }
-
-
-
-// const changeSLide = () => {
-
-//     if (currentActiveSection.classList.contains('screenshots')) {
-//         document.querySelector('.wrapper_screenshot_imgs').style.display = 'flex';
-//         if (slider) {
-//             const widthTransform = Number(slider.offsetWidth);
-
-//             if (checkLastActive()) slider.style.transform = `translateX(0px)`;
-
-//             else {
-//                 slider.style.transform = `translateX(-${Number(slider.style.transform.replace(/[^\d.]/g, '')) + widthTransform}px)`;
-
-//             }
-//         }
-//     }
-// }
-
-// let interval = setInterval(() => {
-//     changeSLide()
-// }, 3000);
-
-
-// document.querySelector('section.screenshots').onload = function () { setActiveControl() };
-// function setActiveControl() {
-//     document.querySelector('.scr_active').classList.remove('scr_active');
-//     elNavs[0].classList.add('scr_active');
-// }
 
 
 
@@ -73,11 +22,7 @@ for (let i = 0; i < screenshots.length; i++) {
             const widthTransform = Number(slider.offsetWidth);
             slider.style.transform = `translateX(-${i * (widthTransform)}px)`;
         }
-
-        // clearInterval(interval);
-        // interval = setInterval(() => {
-        //     changeSLide()
-        // }, 3000)
+       
     }
 }
 
